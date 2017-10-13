38551ef99a129a0b3db515f1237f5d4ef187c973
Ryan Kopinsky <contact@ryankopinsky.com>
2017-10-13 17:28:59 -0400

    Updated solution

--- tdd_oop_bank/oop_bank/bank_account.rb
+++ tdd_oop_bank/oop_bank/bank_account.rb
@@ -1,26 +1,29 @@
 class BankAccount
   attr_reader :balance
-  @@minimum_balance = 200
+  @@minimum_opening_balance = 200
 
   def initialize(opening_balance, account_holder)
-    raise ArgumentError if opening_balance < @@minimum_balance
+    if opening_balance < @@minimum_opening_balance
+      raise ArgumentError
+    end
+
     @balance = opening_balance
   end
 
-  def deposit amount
-    @balance += amount
+  def deposit(amount)
+    @balance += amount # @balance = @balance + amount
   end
 
-  def withdraw amount
+  def withdraw(amount)
     @balance -= amount
   end
 
-  def transfer amount, account
-    withdraw amount
-    account.deposit amount
+  def transfer(amount, other_account)
+    withdraw(amount)
+    other_account.deposit(amount)
   end
 
-  def self.minimum_balance= amount
-    @@minimum_balance = amount
+  def self.update_opening_balance(amount)
+    @@minimum_opening_balance = amount
   end
 end
--- tdd_oop_bank/oop_bank/spec/bank_account_spec.rb
+++ tdd_oop_bank/oop_bank/spec/bank_account_spec.rb
@@ -1,7 +1,7 @@
 require './bank_account'
 
 describe BankAccount do
-  it "is created with an opening balance and the name of the client" do
+  it "is created with an balance and the name of the client" do
     account = BankAccount.new(500, "Sarah")
     expect(account).to be_a(BankAccount)
   end
@@ -13,30 +13,32 @@ describe BankAccount do
 
   it "can make deposits" do
     account = BankAccount.new(500, "Sarah")
-    account.deposit(500)
-    expect(account.balance).to eq(1000)
+    account.deposit(250)
+    expect(account.balance).to eq(750)
   end
 
   it "can make withdrawals" do
     account = BankAccount.new(500, "Sarah")
-    account.withdraw(200)
-    expect(account.balance).to eq(300)
+    account.withdraw(100)
+    expect(account.balance).to eq(400)
   end
 
-  it "can make withdrawals" do
-    account1 = BankAccount.new(500, "Sarah")
-    account2 = BankAccount.new(500, "John")
-    account1.transfer(200, account2)
-    expect(account1.balance).to eq(300)
-    expect(account2.balance).to eq(700)
+  it "can transfer funds to another bank account" do
+    account_one = BankAccount.new(500, "Sarah")
+    account_two = BankAccount.new(500, "Beyonce")
+
+    account_one.transfer(200, account_two)
+
+    expect(account_one.balance).to eq(300)
+    expect(account_two.balance).to eq(700)
   end
 
   it "throws an error if minimum opening balance of 200 is not met" do
     expect { BankAccount.new(100, "Sarah") }.to raise_error(ArgumentError)
   end
 
-  it "allows user to configure minimum balance" do
-    BankAccount.minimum_balance = 500
+  it "allows the user to change the minimum balance" do
+    BankAccount.update_opening_balance(500)
     expect { BankAccount.new(300, "Sarah") }.to raise_error(ArgumentError)
     expect { BankAccount.new(500, "Sarah") }.to_not raise_error
   end
